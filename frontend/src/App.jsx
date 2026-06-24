3f32220fa60505abd70e2cf53b08afcfface3947
Emmanuel Oduor <wyllademanu003@gmail.com>
2026-06-24 17:13:53 +0300

    initial commit

--- frontend/src/App.jsx
+++ frontend/src/App.jsx
@@ -1,12 +1,13 @@
 import React from "react";
 import ReactDOM from "react-dom/client";
-import { BrowserRouter, Routes, Route, Navigate } from "react-router-dom";
+import { BrowserRouter, Routes, Route, Navigate, useLocation } from "react-router-dom";
 import Login from "./Login";
 import Dashboard from "./Dashboard";
 import ProjectPage from "./ProjectPage";
 import "./App.css";
 
 function App() {
+  useLocation();
   const token = localStorage.getItem("token");
   return (
     <Routes>
